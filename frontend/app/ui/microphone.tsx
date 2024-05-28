10f5c43a158fbfe3972199bf0d4665be1725ab80
William Littlefield <williamjlittlefield@gmail.com>
2024-05-28 23:43:11 +0800

    Update microphone component to display text

--- frontend/app/ui/microphone.tsx
+++ frontend/app/ui/microphone.tsx
@@ -4,20 +4,23 @@ import { useRecordVoice } from "./voice-recorder";
 import { IconMicrophone } from "./icon-microphone";
 
 export default function Microphone() {
-  const { startRecording, stopRecording } = useRecordVoice();
+  const { startRecording, stopRecording, text } = useRecordVoice();
 
   return (
     // Button for starting and stopping voice recording
-    <button
-      onMouseDown={startRecording}    // Start recording when mouse is pressed
-      onMouseUp={stopRecording}        // Stop recording when mouse is released
-      onTouchStart={startRecording}    // Start recording when touch begins on a touch device
-      onTouchEnd={stopRecording}        // Stop recording when touch ends on a touch device
-      className="border-none bg-transparent w-10"
-    >
-      {/* Microphone icon component */}
-      <IconMicrophone />
-    </button>
+    <div className="flex flex-col justify-center items-center">
+      <button
+        onMouseDown={startRecording}    // Start recording when mouse is pressed
+        onMouseUp={stopRecording}        // Stop recording when mouse is released
+        onTouchStart={startRecording}    // Start recording when touch begins on a touch device
+        onTouchEnd={stopRecording}        // Stop recording when touch ends on a touch device
+        className="border-none bg-transparent w-10"
+      >
+        {/* Microphone icon component */}
+        <IconMicrophone />
+      </button>
+      <p className="text-white">{text}</p>
+    </div >
   );
 };
 
