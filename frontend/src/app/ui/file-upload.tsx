0d527eeaca5618997617704a303e91d4671a9abc
William Littlefield <williamjlittlefield@gmail.com>
2024-06-10 18:53:10 +0800

    Create file upload ui component

---
+++ frontend/src/app/ui/file-upload.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FileUpload(){
+    return (
+        <div>
+            <input type="file" className="file-input file-input-bordered file-input-primary w-full max-w-xs" />
+        </div>
+    );
+}
