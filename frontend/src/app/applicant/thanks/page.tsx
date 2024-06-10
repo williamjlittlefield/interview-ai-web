d34a2ca433bc00b07c4339e6465c04a02b56aca4
William Littlefield <williamjlittlefield@gmail.com>
2024-06-10 21:36:14 +0800

    Scaffold applicant thanks page

---
+++ frontend/src/app/applicant/thanks/page.tsx
@@ -0,0 +1,17 @@
+import Footer from '../../ui/footer';
+import Header from '../../ui/header';
+
+export default function Thanks() {
+    
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+        <Header />
+       
+        <h1>Thank you for using Interview AI!</h1>
+
+        <Footer />      
+        
+    </main>
+  );
+}
